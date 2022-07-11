7af4238cbd33e54e6978fa383cdb1042d1176c07
Maximiliano Contreras <100421513+pablomcontreras@users.noreply.github.com>
2022-07-10 22:22:00 -0300

    TERMINADO sin autenticacion

--- src/app/productos/editar-producto/editar-producto.component.ts
+++ src/app/productos/editar-producto/editar-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/services/producto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-producto',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-producto.component.css']
 })
 export class EditarProductoComponent implements OnInit {
+  form: any = {};
+  actualizado = false;
+  failActualizado = false;
+  msjErr = '';
+  msjOK = '';
 
-  constructor() { }
+  failInit = false;
 
-  ngOnInit(): void {
+  constructor(private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.productoService.detalle(id).subscribe( data => {
+      this.form.nombreProducto = data.nombreProducto;
+      this.form.precio = data.precio;
+    },
+      (err: any) => {
+        this.failInit = true;
+        this.router.navigate(['']);
+      }
+    );
+  }
+
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.productoService.editar(this.form, id).subscribe( data => {
+      this.actualizado = true;
+      this.failActualizado = false;
+      this.msjOK = data.mensaje;
+    },
+    (err: any) => {
+      this.actualizado = false;
+      this.failActualizado = true;
+      this.msjErr = err.error.mensaje;
+    }
+    );
+  }
+
+  volver(): void {
+    window.history.back();
   }
 
 }
--- src/app/productos/nuevo-producto/nuevo-producto.component.ts
+++ src/app/productos/nuevo-producto/nuevo-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-nuevo-producto',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-producto.component.css']
 })
 export class NuevoProductoComponent implements OnInit {
+  form: any = {};
+  producto: Producto| null = null;
+  creado = false;
+  failProducto = false;
+  mensajeFail = '';
+  mensajeOK = '';
 
-  constructor() { }
+  constructor(private productoService: ProductoService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onCreate(): void {
+    this.productoService.crear(this.form).subscribe(data => {
+      this.mensajeOK = data.mensaje;
+      this.creado = true;
+      this.failProducto = false;
+    },
+      (err: any) => {
+        this.mensajeFail = err.error.mensaje;
+        this.creado = false;
+        this.failProducto = true;
+      }
+    );
+  }
+
+  volver(): void {
+    window.history.back();
   }
 
 }
