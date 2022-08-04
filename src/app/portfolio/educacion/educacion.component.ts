ac17b10d88fe9925c2b8d735abb700795e60f167
Maximiliano Contreras <100421513+pablomcontreras@users.noreply.github.com>
2022-08-04 13:24:30 -0300

    agregadas alertas de educación

--- src/app/portfolio/educacion/educacion.component.ts
+++ src/app/portfolio/educacion/educacion.component.ts
@@ -3,6 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EducacionService } from 'src/app/services/educacion.service';
 import { AgregarEducacionComponent } from './agregar-educacion/agregar-educacion.component';
 import { EditarEducacionComponent } from './editar-educacion/editar-educacion.component';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-educacion',
@@ -31,6 +32,15 @@ export class EducacionComponent implements OnInit {
 
     modalRef.result.then((result) => {
       this.datosEducacion.crear(result).subscribe((data) => {
+        Swal.fire({
+          title: 'Exito!',
+          text: 'La educación fue agregada con éxito',
+          icon: 'success',
+          confirmButtonText: 'Volver',
+          buttonsStyling: false,
+          customClass: {
+    	      confirmButton: 'btn btn-success'
+      }})
         this.cargarLista();
       });
     });
@@ -49,18 +59,47 @@ export class EducacionComponent implements OnInit {
 
     modalRef.result.then((result) => {
       this.datosEducacion.editar(result, id).subscribe((data) => {
+        Swal.fire({
+          title: 'Exito!',
+          text: 'La educación fue actualizada con éxito',
+          icon: 'success',
+          confirmButtonText: 'Volver',
+          buttonsStyling: false,
+          customClass: {
+    	      confirmButton: 'btn btn-success'
+      }})
         this.cargarLista();
       });
     });
   }
 
   borrar(id: number): void {
-    if (confirm('¿Estás seguro?')) {
-      this.datosEducacion.borrar(id).subscribe((data) => {
-        this.cargarLista();
-      });
+
+
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "Esta acción no puede deshacerse",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Eliminar',
+      
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Eliminado!',
+          text: 'El registro ha sido eliminado exitosamente.',
+          icon:'success',
+          buttonsStyling: false,
+          customClass: {confirmButton: 'btn btn-success'}
+        });
+        this.datosEducacion.borrar(id).subscribe((data) => {
+          this.cargarLista();
+        });
     }
-  }
+  });}
 
   cargarLista() {
     this.datosEducacion.lista().subscribe((data) => {
